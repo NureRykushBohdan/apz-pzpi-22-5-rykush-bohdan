45ae9d428c9fb7f21b50b15f0b96b801c93eb7d9
NureRykushBohdan <bohdan.rykush@nure.ua>
2025-06-05 20:48:07 +0200

    фикс панельки

--- Lab3/pzpi-22-5-rykush-bohdan-lab3/public/js/front/script.js
+++ Lab3/pzpi-22-5-rykush-bohdan-lab3/public/js/front/script.js
@@ -91,21 +91,5 @@ document.querySelectorAll('.faq-item').forEach(item => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const userNameButton = document.getElementById("user-name");
-    const dropdownMenu = document.getElementById("dropdown-menu");
-
-    if (userNameButton) {
-        userNameButton.addEventListener("click", function () {
-            dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
-        });
-
-        document.addEventListener("click", function (event) {
-            if (!userNameButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
-                dropdownMenu.style.display = "none";
-            }
-        });
-    }
-});
 
 
